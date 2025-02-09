bb168b8d15d084f248d0ccdabd4068b430d3b894
frieswithsalsa <romansedin@hotmail.com>
2025-02-09 15:50:47 +0200

    Set up dev server

--- config/build/buildWebPackConfig.ts
+++ config/build/buildWebPackConfig.ts
@@ -1,12 +1,12 @@
 import { BuildOptions } from "./types/config";
 import webpack from "webpack";
-import path from 'path';
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolves";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode} = options;
+    const {paths, mode, isDev} = options;
     
     return {
         mode: mode,
@@ -21,6 +21,8 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         module: {
             rules: buildLoaders(),
           },
-          resolve: buildResolvers(), 
+          resolve: buildResolvers(),
+          devtool: isDev ? 'inline-source-map' : undefined,
+          devServer: isDev ? buildDevServer(options) : undefined,
       }
 }
