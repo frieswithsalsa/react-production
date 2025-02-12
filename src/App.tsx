2c1c6851072e8fb2da7c95293891b178d92386aa
frieswithsalsa <romansedin@hotmail.com>
2025-02-12 18:00:01 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import { Routes } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import './index.scss'
-import MainPage from './pages/MainPage/MainPage'
-import AboutPage from './pages/AboutPage/AboutPage'
+import { lazy, Suspense } from 'react';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
 
 
 const App = () => {
   return (
     <div className="app">
-      <Routes>
-        <Route path={'/about'} element={<AboutPage />}/>
-        <Route path={'/'} element={<MainPage />}/>
-      </Routes>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path={'/about'} element={<AboutPageAsync />}/>
+            <Route path={'/'} element={<MainPageAsync />}/>
+          </Routes>
+        </Suspense>
     </div>
   )
 }
