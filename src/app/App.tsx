b8737eec7edb1c78e6b2d4a83d2e3f2f5a6eee80
frieswithsalsa <romansedin@hotmail.com>
2025-03-07 13:51:18 +0200

    add stylelint, i18n plugin for eslint, fix all styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,26 @@
-import './styles/index.scss';
-import { useTheme } from 'app/providers/ThemeProvider';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { Navbar } from 'widgets/NavBar';
-import { Sidebar } from 'widgets/SideBar/ui';
-import { Suspense } from 'react';
-import { AppRouter } from './providers/router';
+import "./styles/index.scss";
+import { useTheme } from "app/providers/ThemeProvider";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from "widgets/NavBar";
+import { Sidebar } from "widgets/SideBar/ui";
+import { Suspense } from "react";
+import { AppRouter } from "./providers/router";
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <Navbar />
-                <div className="content-page">
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Suspense fallback="">
+        <Navbar />
+
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
         </div>
-    );
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
