552422d35612ee55733427d2506103c69160036a
frieswithsalsa <romansedin@hotmail.com>
2025-03-10 21:00:52 +0200

    fix

--- src/widgets/SideBar/ui/SideBar/Sidebar.tsx
+++ src/widgets/SideBar/ui/SideBar/Sidebar.tsx
@@ -4,13 +4,17 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
+    
     const [collapsed, setCollapsed] = useState(false);
+
+    const {t} = useTranslation()
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
@@ -28,7 +32,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 type="button"
                 onClick={onToggle}
             >
-                toggle
+                {t('Переключить')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
